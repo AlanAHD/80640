623da7007d95795d9ef6b5724edaaae20df77449
AlanAHD <alanarmando997@gmail.com>
2022-10-19 12:20:41 -0500

    Agregar-19/10/2022

--- practica5/agregar.js
+++ practica5/agregar.js
@@ -12,18 +12,29 @@ function agregar(){
 }
 
 function buscar(){
+
     //buscar por id
     // if(document.getElementById(leer()) == null)
     // console.log("No existe")
     // else
     // alert("Ojo! Ya existe")
     listaTodo=document.getElementsByTagName("li")
-    for(const element of listaTodo){
-        if(element.textContent == leer()){
-            console.log("Si Existe", element)
-        }else{
-            console.log("No existe",element)
+    for(let tarea in listaTodo){
+        if(Object.hasOwnProperty.call(listaTodo,tarea)){
+
+            a=listaTodo[tarea].childNodes[0].valueOf()
+            b=leer()
+            console.log("a",typeof a.textContext, "b",typeof b)
+            if(a==b)
+            console.log("Ya existe")
         }
     }
+    // for(const element of listaTodo){
+    //     if(element.textContent == leer()){
+    //         console.log("Si Existe", element)
+    //     }else{
+    //         console.log("No existe",element)
+    //     }
+    // }
 
 }
