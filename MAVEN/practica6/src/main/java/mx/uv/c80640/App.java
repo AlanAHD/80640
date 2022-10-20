b533073774b8e4e1e2ff5567f53a7af4952e078b
AlanAHD <alanarmando997@gmail.com>
2022-10-20 12:58:54 -0500

    Practica6 actualizada 20/10/2022

--- MAVEN/practica6/src/main/java/mx/uv/c80640/App.java
+++ MAVEN/practica6/src/main/java/mx/uv/c80640/App.java
@@ -1,4 +1,5 @@
 package mx.uv.c80640;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -9,5 +10,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
+        get("/eduardo", (req, res) -> "Hello Eduardo");
+        get("/alex", (req, res) -> "Hello Alex");
+        get("/brandon", (req, res) -> "Hello Brandon");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
+        // get("/", (req, res) -> "Hola");
+
+        // post("/", (req, res) -> {
+        //     System.out.println(req.queryParams(null));
+        //     System.out.println(req.body());
+        // });
+        
     }
 }
