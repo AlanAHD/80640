d3254758a6e4bf764b84934767bb5a77cf28f8f5
AlanAHD <alanarmando997@gmail.com>
2022-11-03 13:03:48 -0600

    Actualizacion 03/11/22

--- MAVEN/practica6/src/main/java/mx/uv/c80640/App.java
+++ MAVEN/practica6/src/main/java/mx/uv/c80640/App.java
@@ -1,5 +1,7 @@
 package mx.uv.c80640;
 import static spark.Spark.*;
+import com.google.gson.JsonObject;
+
 
 /**
  * Hello world!
@@ -15,8 +17,18 @@ public class App
         get("/alex", (req, res) -> "Hello Alex");
         get("/brandon", (req, res) -> "Hello Brandon");
         get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
-        // get("/", (req, res) -> "Hola");
+        get("/", (req, res) -> "Hola");
 
+        post("/", (req, res) ->{
+            System.out.println(req.queryParams("email") + " " + 
+            req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta = new JsonObject();
+            oRespuesta.addProperty("msj", "hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
+            return oRespuesta;
+        });
         // post("/", (req, res) -> {
         //     System.out.println(req.queryParams(null));
         //     System.out.println(req.body());
